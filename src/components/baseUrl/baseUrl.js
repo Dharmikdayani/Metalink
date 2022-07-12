f3ec0f3699babde9499ed25f06bb1dd2029a72f1
Dharmikdayani <dayanidharmik42@gmail.com>
2022-07-12 16:21:46 +0530

    api chnag

--- src/components/baseUrl/baseUrl.js
+++ src/components/baseUrl/baseUrl.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
-    // https://metalink-technomads.herokuapp.com/
-    // http://localhost:3000
-    withCredentials:true
-  });
+  baseURL: "https://metalink-technomads.herokuapp.com",
+  // https://metalink-technomads.herokuapp.com/
+  // http://localhost:3000
+  withCredentials: true,
+});
 
-export default instance
+export default instance;
