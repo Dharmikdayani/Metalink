1deff537f87978a8f509d6143bfcb859c8bb7df7
Dharmikdayani <dayanidharmik42@gmail.com>
2022-07-12 11:20:11 +0530

    link change

--- src/components/baseUrl/baseUrl.js
+++ src/components/baseUrl/baseUrl.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://metalink-technomads.herokuapp.com',
+    // https://metalink-technomads.herokuapp.com/
+    // http://localhost:3000
     withCredentials:true
   });
 
