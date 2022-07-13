d94186222a6e2a2301c4fcbf3193e7be8c05fbe6
Dharmikdayani <dayanidharmik42@gmail.com>
2022-07-13 11:15:49 +0530

    done app

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const App = () => {
     setCurrentBalance({});
     // console.log("first",user)
     if (getItem) {
-      const socket = io("http://localhost:3000");
+      const socket = io("https://metalink-technomads.herokuapp.com");
       socket.on("connect", () => {
         setSocket(socket);
         // console.log( getItem._id)
